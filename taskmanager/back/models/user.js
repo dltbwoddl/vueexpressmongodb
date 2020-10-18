3f3b70011a51fbec0ee94fbf24016d2b61636e70
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-10-18 19:38:17 +0900

    1018
    1018

---
+++ taskmanager/back/models/user.js
@@ -0,0 +1,19 @@
+import mongoose, { Schema } from 'mongoose';
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    email:{
+        type : String,
+        required : true
+    },
+    password : {
+        type : String,
+        required : true
+    },
+    name:{
+        type:String,
+        required:true
+    }
+});
+
+export default mongoose.model('UserSchema')
