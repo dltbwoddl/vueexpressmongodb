611f054722d73b0c45fffb174f40ccecc304790b
dltbwoddl <60048456+dltbwoddl@users.noreply.github.com>
2020-10-22 15:39:03 +0900

    1022
    1022

--- taskmanager/back/app.js
+++ taskmanager/back/app.js
@@ -1,55 +1,84 @@
 const express = require('express');
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
-const {body, validationResult} = require('express-validator')
+const { body, validationResult } = require("express-validator");
+const bcrypt = require('bcryptjs');
+
+const User = require('./models/user');
+
 dotenv.config();
-
 const app = express();
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
-app.post('/auth/signup',[
-    body('email')
+app.post('/auth/signup', [
+  body('email')
     .isEmail()
-    .withMessage("please valid email"),
-    body('password')
+    .withMessage('Please type valid email')
+    .custom((value) => {
+        return User.findOne({ email: value }).then((userDoc) => {
+            if (userDoc) {
+                return Promise.reject('E-Mail address already exists!');
+            }
+        });
+    }),
+  body('password')
     .trim()
-    .isLength({min:6})
-    .withMessage("password must be greater than 6 characters")
-    ,body('name')
+    .isLength({ min: 6 })
+    .withMessage('Password must be greater than 6 characters'),
+  body('name')
     .trim()
     .not().isEmpty()
-    .withMessage('None field is required')
-],(req,res,next)=>{
-    const errors=validationResult(req);
+    .withMessage('Name field is required')
+], async (req, res, next) => {
+    const errors = validationResult(req);
 
-    if (!errors.isEmpty()){
-        const error = new Error('validation failed')
-        error.statusCode=422;
-        error.data=errors.array();
-        throw error;
+    if (!errors.isEmpty()) {
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        error.data = errors.array();
+        return next(error);
     }
-    console.log(req.body)
     const email = req.body.email;
-    const password =req.body.password;
-    const name =req.body.name;
-    res.json({
-        message: 'success'
-    });
-})
+    const password = req.body.password;
+    const name = req.body.name;
 
-app.use((error,req,res,next)=>{
-    const status = error.statusCode || 500; 
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12);
+
+        const user = new User({
+            email,
+            password: hashedPassword,
+            name
+        });
+
+        //
+        const result = await user.save();
+        res.status(201).json({
+            message: "User created",
+            user: result
+        })
+    } catch (err) {
+       if (!err.statusCode) {
+           err.statusCode = 500;
+       }
+       next(err);
+    }
+});
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500;
     const message = error.message;
     const data = error.data;
     res.status(status).json({
-        message,data
+        message, data
     })
-});
+})
+
 
 mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.vwjt7.mongodb.net/limproject?retryWrites=true&w=majority`)
-.then(()=>{
-    app.listen(3000,()=>{
-        console.log('listen t0 3000')
-    })
-}).catch( err => console.log(err) )
+    .then(() => {
+        app.listen(3000, () => {
+            console.log('listen t0 3000')
+        })
+    }).catch(err => console.log(err))
--- taskmanager/back/models/user.js
+++ taskmanager/back/models/user.js
@@ -1,19 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema({
-    email:{
-        type : String,
-        required : true
-    },
-    password : {
-        type : String,
-        required : true
-    },
-    name:{
-        type:String,
-        required:true
-    }
+const userSchema = new Schema({
+	email: {
+		type: String,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	name: {
+		type: String,
+		required: true
+	},
 });
 
-module.exports =  mongoose.model('UserSchema')
+module.exports = mongoose.model('User', userSchema);
